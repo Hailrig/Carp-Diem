fab521bc7ce177baf746a4fdc28111e412267a7a
Hailrig <shrike321@hotmail.com>
2018-11-03 00:52:28 -0500

    Fixed positions of hands

--- Characters/Characters.gd
+++ Characters/Characters.gd
@@ -9,6 +9,7 @@ export (int) var speed
 export (float) var weapon_cooldown
 export (int) var max_health
 export (int) var starting_health
+export (int) var weapon_offset
 
 export (String) var front_idle
 export (String) var front_right_idle
@@ -50,7 +51,7 @@ func _physics_process(delta):
 		return
 	control(delta)
 	move_and_slide(velocity)
-	change_anim($Weapon.global_rotation_degrees, velocity)
+	change_anim(rad2deg($Body.get_angle_to(get_global_mouse_position())), velocity)
 	
 func change_anim(angle, velocity):
 	if velocity.x == 0 and velocity.y == 0:
@@ -91,6 +92,16 @@ func change_anim(angle, velocity):
 		if angle > -45 and angle <= 0 and playing_anim != 6:
 			$Body/AnimationPlayer.play(back_right_walk)
 			playing_anim = 6
+
+	if angle >= 105 or angle <= -105:
+		$Weapon.flip_v = true
+		$Weapon.position.x = -weapon_offset
+		$Arm.position.x = weapon_offset - 3
+	elif angle <= 75 and angle >= -75:
+		$Weapon.flip_v = false
+		$Weapon.position.x = weapon_offset
+		$Arm.position.x = -weapon_offset + 3
+		
 
 	
 func take_damage(amount):

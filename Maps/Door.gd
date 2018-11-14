3773223ed2638199f10b76e1d6d4a6ec922539fc
Hailrig <shrike321@hotmail.com>
2018-11-14 13:50:40 -0600

    Inital implement

---
+++ Maps/Door.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+#
+#
+#func _on_Door_body_entered(body):
+#	if body.name == "Player":
+#		_open()
+#
+#
+#
+#func _on_Door_body_exited(body):
+#	if body.name == "Player":
+#		_close()
+#
+#func _open():
+#	print("open ya booty")
+#
+#func _close():
+#	print("close ya booty")

d039015e1d68b9a19fd39fec2e40cef586814280
Hailrig <shrike321@hotmail.com>
2018-11-23 00:14:27 -0600

    Fully integrated doors.

--- Maps/Door.gd
+++ Maps/Door.gd
@@ -6,15 +6,90 @@ export (String) var room
 export (bool) var locked
 export (int) var keys
 export (bool) var enemyless
+export (String) var enter_dir
+export (bool) var z_change
+
+export (String) var open_behind
+export (String) var open_front
+export (String) var close_behind
+export (String) var close_front
 
 var door_stop = false
 
-#func _ready():
-#	connecter()
+func _ready():
+	if enter_dir == "east":
+		$Area2D/CollisionShape2D.position.x = -9.74668
+		$Area2D/CollisionShape2D.position.y = 0.86022
+		$Area2D/CollisionShape2D.scale.x = 1
+		$Area2D/CollisionShape2D.scale.y = 1.67219
+		
+		$Area2D2/CollisionShape2D.position.x = 42.113918
+		$Area2D2/CollisionShape2D.position.y = -1.58433
+		$Area2D2/CollisionShape2D.scale.x = 1
+		$Area2D2/CollisionShape2D.scale.y = 1.67219
+		
+		$CollisionShape2D.position.x = 0
+		$CollisionShape2D.position.y = 0
+		$CollisionShape2D.scale.x = 1
+		$CollisionShape2D.scale.y = 1
+	if enter_dir == "west":
+		$Area2D/CollisionShape2D.position.x = 47.693378
+		$Area2D/CollisionShape2D.position.y = -0.891688
+		$Area2D/CollisionShape2D.scale.x = 1
+		$Area2D/CollisionShape2D.scale.y = 1.67219
+		
+		$Area2D2/CollisionShape2D.position.x = -16.858112
+		$Area2D2/CollisionShape2D.position.y = -1.58433
+		$Area2D2/CollisionShape2D.scale.x = 1
+		$Area2D2/CollisionShape2D.scale.y = 1.67219
+		
+		$CollisionShape2D.position.x = 26.570557
+		$CollisionShape2D.position.y = 0
+		$CollisionShape2D.scale.x = 1
+		$CollisionShape2D.scale.y = 1
+	if enter_dir == "north":
+		$Area2D/CollisionShape2D.position.x = 36.423717
+		$Area2D/CollisionShape2D.position.y = 27.738504
+		$Area2D/CollisionShape2D.scale.x = 0.1
+		$Area2D/CollisionShape2D.scale.y = 12
+		
+		$Area2D2/CollisionShape2D.position.x = 25.980251
+		$Area2D2/CollisionShape2D.position.y = -40.504257
+		$Area2D2/CollisionShape2D.scale.x = 12
+		$Area2D2/CollisionShape2D.scale.y = 0.1
+		
+		$CollisionShape2D.position.x = 29.965691
+		$CollisionShape2D.position.y = 12.128963
+		$CollisionShape2D.scale.x = 12
+		$CollisionShape2D.scale.y = 0.05
+		
+		$FrontDoor.visible = false
+	if enter_dir == "south":
+		$Area2D/CollisionShape2D.position.x = 36.423717
+		$Area2D/CollisionShape2D.position.y = -33.144161
+		$Area2D/CollisionShape2D.scale.x = 0.2
+		$Area2D/CollisionShape2D.scale.y = 12
+		
+		$Area2D2/CollisionShape2D.position.x = 25.980251
+		$Area2D2/CollisionShape2D.position.y = 23.558338
+		$Area2D2/CollisionShape2D.scale.x = 12
+		$Area2D2/CollisionShape2D.scale.y = 0.1
+		
+		$CollisionShape2D.position.x = 29.965691
+		$CollisionShape2D.position.y = -18.164143
+		$CollisionShape2D.scale.x = 12
+		$CollisionShape2D.scale.y = 0.05
+		
+		$FrontDoor.visible = false
+	
+	if z_change:
+		$BehindDoor.z_index = 5
+	
+	$BehindDoor/AnimationPlayer.play(close_behind)
+	$FrontDoor/AnimationPlayer.play(close_front)
 
 func unlock():
 	keys -= 1
-	print (keys)
 	if keys <= 0:
 		locked = false
 
@@ -37,19 +112,24 @@ func _open():
 		emit_signal('room_entered')
 		perma_open()
 	elif !door_stop and !locked:
+		print("fuckyou")
 		$CollisionShape2D.disabled = true
-		$Sprite/AnimationPlayer.play('door_open')
+		$BehindDoor/AnimationPlayer.play(open_behind)
+		$FrontDoor/AnimationPlayer.play(open_front)
 		$AudioStreamPlayer2D.play()
 
 func _close():
 	if !door_stop and !locked:
+		print("fuckme")
 		$CollisionShape2D.disabled = false
-		$Sprite/AnimationPlayer.play('door_close')
+		$BehindDoor/AnimationPlayer.play(close_behind)
+		$FrontDoor/AnimationPlayer.play(close_front)
 		$AudioStreamPlayer2D.play()
 	
 func perma_open():
 	$CollisionShape2D.disabled = true
-	$Sprite/AnimationPlayer.play('door_open')
+	$BehindDoor/AnimationPlayer.play(open_behind)
+	$FrontDoor/AnimationPlayer.play(open_front)
 	door_stop=true
 	$AudioStreamPlayer2D.play()
 	$Area2D.queue_free()

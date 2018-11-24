e9f3452c925fe59dc0fb4c5788c22f3368143408
Hailrig <shrike321@hotmail.com>
2018-11-24 02:35:10 -0600

    Mixing boss moves

--- Characters/BuffPuff.gd
+++ Characters/BuffPuff.gd
@@ -1,6 +1,7 @@
 extends "res://Characters/EvilFish.gd"
 
 var boss_bar
+var fire_mode = null
 
 func shoot():
 #		if _in_clip >= 0:
@@ -11,6 +12,14 @@ func shoot():
 			emit_signal('ammo_changed', _in_clip)
 			can_shoot = false
 			$WeaponTimer.start()
+			
+			
+			if health >= 80:
+				tri_shot()
+			elif fire_mode == 0:
+				tri_shot()
+			elif fire_mode == 1:
+				spin_shot()
 #			var dir = Vector2(1, 0).rotated($Weapon.global_rotation)
 #			emit_signal('shoot', Bullet, $Weapon/Muzzle.global_position, dir)
 #			dir = Vector2(1, 0).rotated($Weapon.global_rotation + .5)
@@ -21,9 +30,20 @@ func shoot():
 #			emit_signal('shoot', Bullet, $Weapon/Muzzle.global_position, dir)
 #			dir = Vector2(1, 0).rotated($Weapon.global_rotation - .25)
 #			emit_signal('shoot', Bullet, $Weapon/Muzzle.global_position, dir)
-			spin_shot()
 		elif $ReloadTimer.time_left == 0:
 			reload()
+
+func tri_shot():
+	var dir = Vector2(1, 0).rotated($Weapon.global_rotation)
+	emit_signal('shoot', Bullet, $Weapon/Muzzle.global_position, dir)
+	dir = Vector2(1, 0).rotated($Weapon.global_rotation + .5)
+	emit_signal('shoot', Bullet, $Weapon/Muzzle.global_position, dir)
+	dir = Vector2(1, 0).rotated($Weapon.global_rotation - .5)
+	emit_signal('shoot', Bullet, $Weapon/Muzzle.global_position, dir)
+	dir = Vector2(1, 0).rotated($Weapon.global_rotation + .25)
+	emit_signal('shoot', Bullet, $Weapon/Muzzle.global_position, dir)
+	dir = Vector2(1, 0).rotated($Weapon.global_rotation - .25)
+	emit_signal('shoot', Bullet, $Weapon/Muzzle.global_position, dir)
 
 func spin_shot():
 	for i in 36:
@@ -70,3 +90,7 @@ func getrekt():
 		remove_from_group(current_room)
 		$Weapon.queue_free()
 		alive = false
+
+func _on_Fireswitch_timeout():
+	fire_mode = randi()%2
+	print(fire_mode)

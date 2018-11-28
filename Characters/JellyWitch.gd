18ae5b1fb5c65373d13d9e5d731de0096bc962e0
Hailrig <shrike321@hotmail.com>
2018-11-28 09:28:28 -0600

    Bundle of fixes

--- Characters/JellyWitch.gd
+++ Characters/JellyWitch.gd
@@ -18,11 +18,11 @@ func tri_shot():
 	i += .05
 	$WeaponTimer.wait_time = 0.05
 	clip_size = 50
-	var dir = Vector2(1, 0).rotated($Weapon.global_rotation + i)
+	var dir = Vector2(1, 0).rotated(i)
 	emit_signal('shoot', Bullet, $Weapon/Muzzle.global_position, dir)
-	dir = Vector2(1, 0).rotated($Weapon.global_rotation + 1.5708 + i)
+	dir = Vector2(1, 0).rotated(1.5708 + i)
 	emit_signal('shoot', Bullet, $Weapon/Muzzle.global_position, dir)
-	dir = Vector2(1, 0).rotated($Weapon.global_rotation + 3.14159 + i)
+	dir = Vector2(1, 0).rotated(3.14159 + i)
 	emit_signal('shoot', Bullet, $Weapon/Muzzle.global_position, dir)
-	dir = Vector2(1, 0).rotated($Weapon.global_rotation + 4.71239 + i)
+	dir = Vector2(1, 0).rotated(4.71239 + i)
 	emit_signal('shoot', Bullet, $Weapon/Muzzle.global_position, dir)
